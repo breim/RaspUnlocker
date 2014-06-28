4fbe32c4c426d90a81b934b2860bbeb4140df7d3
Henrique Breim <henrique@breim.com.br>
2014-06-28 19:29:43 -0300

    Arrumando bug na routes

--- sistema/config/routes.rb
+++ sistema/config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   
 
   root 'testes#index'
-  
+
   get 'ligar' => 'testes#ligar'
-  get 'desligar' => 'testes#ligar'
+  get 'desligar' => 'testes#desligar'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
